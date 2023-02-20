283deff62be253c62934d8f59f3dab2ab5d69363
Evan Seitz <evan.e.seitz@gmail.com>
2023-02-20 00:11:07 -0500

    update

--- 1_LS/AnalyticalForm.py
+++ 1_LS/AnalyticalForm.py
@@ -11,62 +11,13 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import rcParams
 
 
-
 plt.rcParams.update({
     "text.usetex": True,
     "font.family": "serif",
     "font.serif": ["Times New Roman"],
 })
 
-'''states_gt = 20 #ground-truth number of states along each degree of freedom (assumed symmetric)
-tau = 1
-ss_gt = 400
-CM1_idx = np.ndarray(shape=(states_gt, states_gt*tau), dtype=int)  
-CM2_idx = np.ndarray(shape=(states_gt, states_gt*tau), dtype=int)  
 
-# ========================================================================
-# Generate CM1 ground-truth indexing:
-# ========================================================================
-idx=0
-shift=0
-Idx=0
-for i in range(ss_gt):
-    if Idx*tau <= i < (Idx+states_gt)*tau:
-        CM1_idx[shift, idx-Idx*tau] = i
-        idx+=1
-        if idx%(states_gt*tau) == 0:
-            Idx+=states_gt
-            shift+=1
-            
-# ========================================================================
-# Generate CM2 ground-truth indexing:
-# ======================================================================== 
-binsActual = []
-Idx = 0
-for s in range(0,states_gt):
-    state_list = []
-    for r in range(0,states_gt):
-        state_list.append(np.arange(Idx,Idx+tau))
-        Idx+=(states_gt*tau)
-    binsActual.append([item for sublist in state_list for item in sublist])
-    Idx-=(ss_gt-tau)
-
-for i in range(states_gt):
-    for j in range(states_gt*tau):
-        CM2_idx[i,j] = binsActual[i][j]'''
-        
-        
-# ========================================================================
-# Generate analytical manifold:
-# ========================================================================
-
-
-
-#if 1:
-    #a, b = 3,1
-    #z = np.cos(a*np.pi*xx)*np.cos(b*np.pi*yy)
-    
-    
 # =============================================================================
 # Bottom of Figure 4
 # =============================================================================
@@ -79,8 +30,6 @@ if 1:
     fig = plt.figure(figsize=[dimCols*2,dimRows*2], constrained_layout=True)
     gs = GridSpec(dimRows, dimCols, left=0.05, right=0.85, wspace=0.1, hspace=0.1)
     
-    
-    #Ord = [1,8,2,9,3,10,4,11,5,12,6,13,7,14]
     idx_col1 = 0
     for a in np.arange(0,90+1,15):
         ang = -a*np.pi/180
@@ -142,7 +91,6 @@ if 1:
     if 1: #external view
         fig = plt.figure()
         ax = plt.axes(projection='3d')
-        
         ax.scatter(xx, yy, psi, c='white', s=20, linewidths=.5, edgecolor='k')
         ax.set_xlabel('$X$', labelpad=4, fontsize=18)
         ax.set_ylabel('$Y$', labelpad=4, fontsize=18)
@@ -177,7 +125,6 @@ if 1:
         fig.savefig('_figure_assets_/Fig5B1_psi%s.pdf' % psi_label, dpi=600)
         plt.clf()
         plt.close()
-        
         
     if 1: # X-view
         fig = plt.figure()
@@ -222,8 +169,6 @@ if 1:
                 axis._axinfo['tick']['inward_factor'] = 0.0
                 axis._axinfo['tick']['outward_factor'] = 0.0
                 axis.set_pane_color((0.95, 0.95, 0.95))
-
-            
         else:
             ax.view_init(elev=0, azim=-90.1)
         ax.auto_scale_xyz
@@ -271,258 +216,185 @@ if 1:
         plt.close()
 
       
-        
-      
-        
-      
-        
-      
-        
-      
-        
-      
-        
 # =============================================================================
 # Figure 12
 # =============================================================================
-if 0: #FIGURE IN MS: comparing analytical to Figure 3 and Figure 4
-    #a1 = .90
-    #a2 = .99
-    #z1 = (a1)*np.cos(1*np.pi*xx) + (1-a1)*np.cos(2*np.pi*yy)
-    
-    #Psi1: -181, {1,1}, -X, Y
-    #Psi2: -260, {2,1}, X, Y
-    #Psi3: 187, {2,1}, X, -Y
-    #Psi4: 5, {3,1}, X, -Y
-    #Psi5: -92, {4,2}, X, -Y
-    #Psi6:  179, {4,2}, X, Y
+if 1: #FIGURE IN MS: comparing analytical to Figure 3 and Figure 4
+    x = np.linspace(0,1,20)
+    xx, yy = np.meshgrid(x, x)
 
-    th1 = (-181)*np.pi/180
+    th1 = (-177)*np.pi/180
     k,j = 1, 1
     z1 = (np.cos(th1))*np.cos(k*np.pi*(xx)) + (np.sin(th1))*np.cos(j*np.pi*yy)
     
-    th2 = (-260)*np.pi/180
+    th2 = (-100)*np.pi/180
     k,j = 2, 1
     z2 = (np.cos(th2))*np.cos(k*np.pi*(xx)) + (np.sin(th2))*np.cos(j*np.pi*yy)
     
-    th3 = (187)*np.pi/180
+    th3 = (168)*np.pi/180
     k,j = 2, 1
     z3 = (np.cos(th3))*np.cos(k*np.pi*(xx)) + (np.sin(th3))*np.cos(j*np.pi*yy)
     
-    th5 = (-92)*np.pi/180
+    th4 = (-182)*np.pi/180
+    k,j = 3, 1
+    z4 = (np.cos(th4))*np.cos(k*np.pi*(xx)) + (np.sin(th4))*np.cos(j*np.pi*yy)
+    
+    th5 = (-86)*np.pi/180
     k,j = 4, 2
     z5 = (np.cos(th5))*np.cos(k*np.pi*(xx)) + (np.sin(th5))*np.cos(j*np.pi*yy)
     
-    th6 = (179)*np.pi/180
+    th6 = (-177)*np.pi/180 #183
     k,j = 4, 2
     z6 = (np.cos(th6))*np.cos(k*np.pi*(xx)) + (np.sin(th6))*np.cos(j*np.pi*yy)
-
-    psi_label = '6'
-    
-    if 1: # X-view
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
-        ax.scatter(xx, yy, z6, c='white', s=20, linewidths=.5, edgecolor='k')
-        ax.set_xlabel('$X$', labelpad=.5)
-        ax.set_ylabel('$Y$', labelpad=-10)
-        ax.zaxis.set_rotate_label(False)
-        ax.set_zlabel('$\Psi_{%s}$' % psi_label, labelpad=5, rotation=0)
-        ax.xaxis.set_tick_params(labelsize=6)
-        ax.yaxis.set_tick_params(labelsize=6)
-        ax.zaxis.set_tick_params(labelsize=6)
-        ax.tick_params(axis='x', which='major', pad=-1)
-        ax.tick_params(axis='y', which='major', pad=-1)
-        ax.tick_params(axis='z', which='major', pad=3)
-        ax.set_yticks([])
-        ax.w_xaxis.pane.fill = False
-        ax.w_yaxis.pane.fill = False
-        ax.w_zaxis.pane.fill = False
-        ax.xaxis.pane.set_edgecolor('k')
-        ax.yaxis.pane.set_edgecolor('k')
-        ax.zaxis.pane.set_edgecolor('k')
-        ax.xaxis.pane.set_alpha(1)
-        ax.yaxis.pane.set_alpha(1)
-        ax.zaxis.pane.set_alpha(1)
-        ax.xaxis._axinfo["grid"].update({"linewidth":1, "color" : "black"})
-        ax.xaxis._axinfo["grid"]['linewidth'] = .1
-        ax.yaxis._axinfo["grid"].update({"linewidth":1, "color" : "black"})
-        ax.yaxis._axinfo["grid"]['linewidth'] = .1
-        ax.zaxis._axinfo["grid"].update({"linewidth":1, "color" : "black"})
-        ax.zaxis._axinfo["grid"]['linewidth'] = .1
-        #ax.grid(False)
-        ax.view_init(elev=0, azim=-90.1)
-        #ax.view_init(elev=0, azim=89.9)
-
-        ax.auto_scale_xyz
-        plt.show()
         
-    if 1: # Y-view
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
-        ax.scatter(xx, yy, z6, c='white', s=20, linewidths=.5, edgecolor='k')
-        ax.set_xlabel('$X$', labelpad=-10)
-        ax.set_ylabel('$Y$', labelpad=.5)        
-        ax.zaxis.set_rotate_label(False)
-        ax.set_zlabel('$\Psi_{%s}$' % psi_label, labelpad=-2, rotation=0)        
-        ax.xaxis.set_tick_params(labelsize=6)
-        ax.yaxis.set_tick_params(labelsize=6)
-        ax.zaxis.set_tick_params(labelsize=6)
-        ax.tick_params(axis='x', which='major', pad=-1)
-        ax.tick_params(axis='y', which='major', pad=-1)
-        ax.tick_params(axis='z', which='major', pad=0)
-        ax.set_xticks([])
-        ax.w_xaxis.pane.fill = False
-        ax.w_yaxis.pane.fill = False
-        ax.w_zaxis.pane.fill = False
-        ax.xaxis.pane.set_edgecolor('k')
-        ax.yaxis.pane.set_edgecolor('k')
-        ax.zaxis.pane.set_edgecolor('k')
-        ax.xaxis.pane.set_alpha(1)
-        ax.yaxis.pane.set_alpha(1)
-        ax.zaxis.pane.set_alpha(1)
-        ax.xaxis._axinfo["grid"].update({"linewidth":1, "color" : "black"})
-        ax.xaxis._axinfo["grid"]['linewidth'] = .1
-        ax.yaxis._axinfo["grid"].update({"linewidth":1, "color" : "black"})
-        ax.yaxis._axinfo["grid"]['linewidth'] = .1
-        ax.zaxis._axinfo["grid"].update({"linewidth":1, "color" : "black"})
-        ax.zaxis._axinfo["grid"]['linewidth'] = .1
-        #ax.grid(False)
-        ax.view_init(elev=0, azim=0)
-        #ax.view_init(elev=0, azim=180.1)
-        ax.auto_scale_xyz
-        plt.show()
+    fig_idx1 = [1,2,3,4,5,6]
+    for f_idx in fig_idx1:
+        psi_label = str(f_idx)
+        if f_idx == 1:
+            Z = z1
+            rev = True
+        elif f_idx == 2:
+            Z = z2
+            rev = True
+        elif f_idx == 3:
+            Z = z3
+            rev = True
+        elif f_idx == 4:
+            Z = z4
+            rev = False
+        elif f_idx == 5:
+            Z = z5
+            rev = False
+        elif f_idx == 6:
+            Z = z6
+            rev = False#True
         
-    if 1: # composite
-        fS = 16
-        s = 30
-        lw = .5
-        #plt.scatter(z1, z2, s=s, c='white', edgecolor='k', linewidths=lw, zorder=1) #PSI 1, PSI 2
-        #plt.scatter(z1[::-1], z3[::-1], s=s, c='white', edgecolor='k', linewidths=lw, zorder=1) #PSI 1, PSI 3
-        #plt.scatter(z2, z3, s=s, c='white', edgecolor='k', linewidths=lw, zorder=1) #PSI 2, PSI 3
-
+        if 1: #X-view
+            fig = plt.figure()
+            ax = plt.axes(projection='3d')
+            if rev is True:
+                ax.scatter(xx, yy[::-1], Z, c='white', s=20, linewidths=.5, edgecolor='k')
+            else:
+                ax.scatter(xx, yy, Z, c='white', s=20, linewidths=.5, edgecolor='k')
+            #ax.set_xlabel('$X$', labelpad=lpadx, fontsize=18)
+            #ax.set_ylabel('$Y$', labelpad=lpady, fontsize=18)  
+            ax.zaxis.set_rotate_label(False)
+            ax.set_zlabel('$\Psi_{%s}$' % psi_label, labelpad=0, rotation=0, fontsize=18)
+            if 1:
+                frame = plt.gca()
+                frame.axes.xaxis.set_ticklabels([])
+                frame.axes.yaxis.set_ticklabels([])
+                frame.axes.zaxis.set_ticklabels([])
+            else:
+                ax.xaxis.set_tick_params(labelsize=10)
+                ax.yaxis.set_tick_params(labelsize=10)
+                ax.zaxis.set_tick_params(labelsize=10)
+                ax.tick_params(axis='x', which='major', pad=-1)
+                ax.tick_params(axis='y', which='major', pad=-1)
+                ax.tick_params(axis='z', which='major', pad=8)
+            ax.set_yticks([])
+            ax.w_xaxis.pane.fill = False
+            ax.w_yaxis.pane.fill = False
+            ax.w_zaxis.pane.fill = False
+            ax.xaxis.pane.set_edgecolor('k')
+            ax.yaxis.pane.set_edgecolor('k')
+            ax.zaxis.pane.set_edgecolor('k')
+            ax.xaxis.pane.set_alpha(1)
+            ax.yaxis.pane.set_alpha(1)
+            ax.zaxis.pane.set_alpha(1)
+            ax.xaxis._axinfo["grid"].update({"linewidth":1, "color" : "black"})
+            ax.xaxis._axinfo["grid"]['linewidth'] = .1
+            ax.yaxis._axinfo["grid"].update({"linewidth":1, "color" : "black"})
+            ax.yaxis._axinfo["grid"]['linewidth'] = .1
+            ax.zaxis._axinfo["grid"].update({"linewidth":1, "color" : "black"})
+            ax.zaxis._axinfo["grid"]['linewidth'] = .1
+            #ax.grid(False)
+            ax.view_init(elev=0, azim=-90.1)
+            #ax.view_init(elev=0, azim=89.9)
+            ax.auto_scale_xyz
+            #plt.show()
+            fig.savefig('_figure_assets_/Fig12_psi%s_X.pdf' % f_idx, dpi=600)
+            plt.clf()
+            
+        if 1: #Y-view
+            fig = plt.figure()
+            ax = plt.axes(projection='3d')
+            ax.scatter(xx, yy, Z, c='white', s=20, linewidths=.5, edgecolor='k')
+            #ax.set_xlabel('$X$', labelpad=lpadx, fontsize=18)
+            #ax.set_ylabel('$Y$', labelpad=lpady, fontsize=18)        
+            ax.zaxis.set_rotate_label(False)
+            ax.set_zlabel('$\Psi_{%s}$' % psi_label, labelpad=-5, rotation=0, fontsize=18)    
+            if 1:
+                frame = plt.gca()
+                frame.axes.xaxis.set_ticklabels([])
+                frame.axes.yaxis.set_ticklabels([])
+                frame.axes.zaxis.set_ticklabels([])
+            else:
+                ax.xaxis.set_tick_params(labelsize=10)
+                ax.yaxis.set_tick_params(labelsize=10)
+                ax.zaxis.set_tick_params(labelsize=10)
+                ax.tick_params(axis='x', which='major', pad=-1)
+                ax.tick_params(axis='y', which='major', pad=-1)
+                ax.tick_params(axis='z', which='major', pad=3)
+            ax.set_xticks([])
+            ax.w_xaxis.pane.fill = False
+            ax.w_yaxis.pane.fill = False
+            ax.w_zaxis.pane.fill = False
+            ax.xaxis.pane.set_edgecolor('k')
+            ax.yaxis.pane.set_edgecolor('k')
+            ax.zaxis.pane.set_edgecolor('k')
+            ax.xaxis.pane.set_alpha(1)
+            ax.yaxis.pane.set_alpha(1)
+            ax.zaxis.pane.set_alpha(1)
+            ax.xaxis._axinfo["grid"].update({"linewidth":1, "color" : "black"})
+            ax.xaxis._axinfo["grid"]['linewidth'] = .1
+            ax.yaxis._axinfo["grid"].update({"linewidth":1, "color" : "black"})
+            ax.yaxis._axinfo["grid"]['linewidth'] = .1
+            ax.zaxis._axinfo["grid"].update({"linewidth":1, "color" : "black"})
+            ax.zaxis._axinfo["grid"]['linewidth'] = .1
+            #ax.grid(False)
+            ax.view_init(elev=0, azim=0)
+            #ax.view_init(elev=0, azim=180.1)
+            ax.auto_scale_xyz
+            #plt.show()
+            fig.savefig('_figure_assets_/Fig12_psi%s_Y.pdf' % f_idx, dpi=600)
+            plt.clf()
+            
+    fig_idx2a = [1,1,2,2]
+    fig_idx2b = [2,3,3,5]
+    for f_idx in range(4):
+        if f_idx == 0:
+            Z1, Z2 = z1, z2
+        elif f_idx == 1:
+            Z1, Z2 = z1, z3
+        elif f_idx == 2:
+            Z1, Z2 = z2, z3
+        elif f_idx == 3:
+            Z1, Z2 = z2, z5
         
-        plt.scatter(z2, z5, s=s, c='white', edgecolor='k', linewidths=lw, zorder=1)
-
-        plt.xlabel(r'$\Psi_{%s}$' % (2), fontsize=fS+2, labelpad=2.5)
-        plt.ylabel(r'$\Psi_{%s}$' % (5), fontsize=fS+2, labelpad=2.5)
-        
-
-        
-        plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-        plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-        plt.rc('font', size=6)
-        if 1:
-            frame = plt.gca()
-            frame.axes.xaxis.set_ticklabels([])
-            frame.axes.yaxis.set_ticklabels([])
-            plt.gca().set_xticks([])
-            plt.xticks([])
-            plt.gca().set_yticks([])
-            plt.yticks([])
-        else:
-            plt.tick_params(axis="x", labelsize=6)
-            plt.tick_params(axis="y", labelsize=6)
-        plt.gca().set_box_aspect(1)
-    
-        #fig = plt.figure()
-        #ax = plt.axes()
-        #ax.scatter(z1, z2, c='white', s=20, linewidths=.5, edgecolor='k')
-        #ax.set_xlabel('$X$', labelpad=.5)
-        #ax.set_ylabel('$Y$', labelpad=-10)
-        #ax.zaxis.set_rotate_label(False)
-        #ax.set_zlabel('$\Psi_{%s}$' % psi_label, labelpad=5, rotation=0)
-        #ax.xaxis.set_tick_params(labelsize=6)
-        #ax.yaxis.set_tick_params(labelsize=6)
-        #ax.zaxis.set_tick_params(labelsize=6)
-        #ax.tick_params(axis='x', which='major', pad=-1)
-        #ax.tick_params(axis='y', which='major', pad=-1)
-        #ax.tick_params(axis='z', which='major', pad=3)
-        #ax.set_yticks([])
-
-        #ax.auto_scale_xy
-        plt.show()
-    
-    
-    if 0:
-        fig = plt.figure()
-        ax = plt.axes(projection='3d')
-        ax.scatter(z1, z2, z1.T)
-        ax.set_xlabel('$X$')
-        ax.set_ylabel('$Y$')
-        ax.set_zlabel('$Z$')
-        ax.auto_scale_xyz
-        plt.show()
-        
-    if 0:
-        plt.scatter(z1,z2, s=1)
-        plt.xlabel('$Z_{1}$')
-        plt.xlabel('$Z_{2}$')
-        plt.show()
-        
-    if 0:
-        plt.scatter(xx,z)
-        #plt.scatter(yy,z)
-        plt.show()
-        plt.scatter(z,z.T)
-        plt.show()
-        
-        
-    
-if 0: #UNKNOWN
-    A, B = 1, 1
-    z1 = A*np.cos(1*np.pi*xx/1) + B*np.cos(1*np.pi*yy/1)*-2
-    #z2 = A*np.cos(2*np.pi*xx) + B*np.cos(1*np.pi*yy)
-    z2 = A*np.cos(2*np.pi*xx/1) + B*np.cos(2*np.pi*yy/1)
-    
-    if 1:
-        plt.suptitle('$Z_1=%s\mathrm{cos}(1\pi x) + %s\mathrm{cos}(1\pi y)$, $\ Z_2=%s\mathrm{cos}(2\pi x) + %s\mathrm{cos}(2\pi y)$' % (A,B,A,B), y=.81)
-        plt.subplot(1,5,1)
-        plt.scatter(xx,z1, c='C0', s=15, edgecolor='k', linewidths=.1)
-        plt.xlabel('$X$')
-        plt.ylabel('$Z_1$')
-        plt.gca().set_box_aspect(1)
-        
-        plt.subplot(1,5,2)
-        plt.scatter(xx,z2, c='C0', s=15, edgecolor='k', linewidths=.1)
-        plt.xlabel('$X$')
-        plt.ylabel('$Z_2$')
-        plt.gca().set_box_aspect(1)
-    
-        plt.subplot(1,5,3)
-        plt.scatter(yy,z1, c='C1', s=15, edgecolor='k', linewidths=.1)
-        plt.xlabel('$Y$')
-        plt.ylabel('$Z_1$')
-        plt.gca().set_box_aspect(1)
-    
-        plt.subplot(1,5,4)
-        plt.scatter(yy,z2, c='C1', s=15, edgecolor='k', linewidths=.1)
-        plt.xlabel('$Y$')
-        plt.ylabel('$Z_2$')
-        plt.gca().set_box_aspect(1)
-    
-        if 1:
-            plt.subplot(1,5,5)
-            plt.scatter(z1,z2, c='C2', s=15, edgecolor='k', linewidths=.1) #parabola
-            plt.xlabel('$Z_1$')
-            plt.ylabel('$Z_2$')
+        if 1: #composite
+            s = 30
+            lw = .5
+            plt.scatter(Z1, Z2, s=s, c='white', edgecolor='k', linewidths=lw, zorder=1)
+            plt.xlabel(r'$\Psi_{%s}$' % (fig_idx2a[f_idx]), fontsize=20, labelpad=3)
+            plt.ylabel(r'$\Psi_{%s}$' % (fig_idx2b[f_idx]), fontsize=20, labelpad=3)
+            plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+            plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+            plt.rc('font', size=6)
+            if 1:
+                frame = plt.gca()
+                frame.axes.xaxis.set_ticklabels([])
+                frame.axes.yaxis.set_ticklabels([])
+                plt.gca().set_xticks([])
+                plt.xticks([])
+                plt.gca().set_yticks([])
+                plt.yticks([])
+            else:
+                plt.tick_params(axis="x", labelsize=6)
+                plt.tick_params(axis="y", labelsize=6)
             plt.gca().set_box_aspect(1)
-        else:
-            plt.subplot(1,5,5)
-            plt.scatter(z1.T,z2.T, c='C2', s=15, edgecolor='k', linewidths=.1) #parabola
-            plt.xlabel('$Z_1^T$')
-            plt.ylabel('$Z_2^T$')
-            plt.gca().set_box_aspect(1)
+            #plt.show()
+            fig.savefig('_figure_assets_/Fig12_psi%s_psi%s.pdf' % (fig_idx2a[f_idx], fig_idx2b[f_idx]), dpi=600)
+            plt.clf()
         
-        plt.tight_layout()
-        plt.subplots_adjust(left=.05, bottom=0.3, right=.95, top=1, wspace=.25, hspace=.2)
         
-    if 1:
-        plt.scatter(z1,z2, c='C2', s=15, edgecolor='k', linewidths=.1) #parabola
-        plt.xlabel('$Z_1$')
-        plt.ylabel('$Z_2$')
-        plt.gca().set_box_aspect(1)
-
-    plt.show()
-    
-
-
